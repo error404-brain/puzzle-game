5e554ffe32949fcdd9f1bb16af88bb4397df9a9f
Hưng Dõ <vokhanhhung03@gmail.com>
2024-12-24 10:28:21 +0700

    Rename classLibrary

---
+++ Model/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassLibrary1
+{
+    public class Piece
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+
+        public Piece(string name, string imagePath)
+        {
+            Name = name;
+            ImagePath = imagePath;
+        }
+    }
+
+}
